53ca283a193215dcba66b4ee9fad381950f9e344
Nate Smith <nate@theinternate.com>
2025-09-11 19:12:31 -0400

    A working installer script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-curl -fsSL https://install.determinate.systems/nix |
-  sh -s -- install linux --no-confirm --init none --determinate
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
+  --determinate \
+  --extra-conf "sandbox = false" \
+  --init none \
+  --no-confirm
+
+sudo chown -R wsdev:wsdev /nix
 
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
-nix run home-manager/master -- switch --flake .
+USER=wsdev nix run home-manager/master -- switch --flake .
